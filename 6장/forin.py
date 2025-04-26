7f99b3d5a51ca9ce0f02f43c56e401871d28b57c
omea92 <thdjun92@naver.com>
2025-04-26 20:00:02 +0900

    반복문 6장 실습

---
+++ 6장/forin.py
@@ -0,0 +1,5 @@
+for waiting_no in [1, 2, 3, 4, 5]:
+    print(waiting_no)
+
+for round in range(11, 15, 2):
+    print(round)    
---
+++ 6장/if.py
@@ -0,0 +1,13 @@
+weather = "미세먼"
+
+if weather == "비" :
+    print("우산 챙겨라")
+    print("바닥 조심해라")
+elif weather == "미세먼지" :
+    print("마스크 써라")    
+else:
+    print("귀찮아....")
+print("종료")    
+
+title = input("오늘 주제는 먼가요?")
+print(title)
