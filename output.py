525cc3e98685a15bf4692dbbd58e65a736c3e01a
omea92 <thdjun92@naver.com>
2025-04-27 15:05:33 +0900

    출력문 실습

---
+++ output.py
@@ -0,0 +1,18 @@
+print("# 하나만 출력합니다.")
+print("Hello Python Programming...!")
+print()
+
+print("# 여러개를 출력합니다.")
+print(10, 20, 30, 40, 50)
+print("안녕하세요", "저의", "이름은", "철수입니다.")
+print() # 줄바꿈
+
+print("이름\t나이\t지역")
+print()
+
+print("안녕" + "하세요")
+# print("안녕" + 1) # 타입다르면 에러난다
+print(3 * "안녕하세요") # 숫자만큼 출력
+print()
+
+print(len("안녕하세요"))
